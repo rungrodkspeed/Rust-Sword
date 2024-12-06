6b11cf001d8b0fd285fe40ba96ee2a4df13d2aa5
rungrodks <rungrodks@hotmail.com>
2024-12-06 16:30:16 +0700

    hello-crabby!

--- hello-crabby/target/debug/deps/hello_crabby-777bedc2cfcb86ed.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/Rust-Sword/hello-crabby/target/debug/deps/hello_crabby-777bedc2cfcb86ed: src/main.rs
-
-/mnt/d/Rust-Sword/hello-crabby/target/debug/deps/hello_crabby-777bedc2cfcb86ed.d: src/main.rs
-
-src/main.rs:
--- hello-crabby/target/debug/hello-crabby.d
+++
@@ -1,1 +0,0 @@
-/mnt/d/Rust-Sword/hello-crabby/target/debug/hello-crabby: /mnt/d/Rust-Sword/hello-crabby/src/main.rs
