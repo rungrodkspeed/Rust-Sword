754eb51f00a118e3332885dd99e3bf11aad1b73e
rungrodks <rungrodks@hotmail.com>
2024-12-08 00:14:25 +0700

    threading

---
+++ threading/src/main.rs
@@ -0,0 +1,38 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+fn main() {
+    let crabby_gold = Arc::new(Mutex::new(10));
+
+    let loot_1 = thread::spawn({
+        let crabby_gold_artifact = Arc::clone(&crabby_gold);
+        move || {
+            let mut gold = crabby_gold_artifact.lock().unwrap();
+            *gold += 100;
+        }
+    });
+
+    let loot_2 = thread::spawn({
+        let crabby_gold_artifact = Arc::clone(&crabby_gold);
+        move || {
+            let mut gold = crabby_gold_artifact.lock().unwrap();
+            *gold += 200;
+        }
+    });
+
+    let loot_3 = thread::spawn({
+        let crabby_gold_artifact = Arc::clone(&crabby_gold);
+        move || {
+            let mut gold = crabby_gold_artifact.lock().unwrap();
+            *gold += 80;
+        }
+    });
+
+    loot_1.join().unwrap();
+    loot_2.join().unwrap();
+    loot_3.join().unwrap();
+
+    print!("Gold: {}", crabby_gold.lock().unwrap());
+}
