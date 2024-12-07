a90ceaf8126f1f8c0f1a0576d72fde970fb9bec6
rungrodks <rungrodks@hotmail.com>
2024-12-08 00:29:51 +0700

    channels

---
+++ channels/src/main.rs
@@ -0,0 +1,26 @@
+use std::sync::{mpsc, Arc};
+use std::thread;
+
+fn main() {
+    let loots = vec![10, 20, 30];
+    let mut crabby_gold_coin = 100;
+
+    let (sender, receiver) = mpsc::sync_channel(loots.len());
+    let sender_arc = Arc::new(sender);
+
+    for loot in loots.clone().into_iter() {
+        thread::spawn({
+            let sender = Arc::clone(&sender_arc);
+            move || {
+                sender.send(loot).unwrap();
+            }
+        });
+    }
+
+    for _ in 0..loots.len() {
+        let loot = receiver.recv().unwrap();
+        crabby_gold_coin += loot;
+    }
+
+    println!("Final Gold: {}", crabby_gold_coin);
+}
